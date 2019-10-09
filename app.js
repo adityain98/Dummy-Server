01398b712d00663699cb45b739093eab140609d8
adityain98 <adityain98@gmail.com>
2019-10-09 11:57:17 +0700

    atlas

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-// mongoose.connect(mongoAtlas, {useNewUrlParser: true, useUnifiedTopology: true})
-//   .then(()=>{
-//     console.log('masuk');
-//   })
-//   .catch(err=>{
-//     console.log(err);
-//   })
+mongoose.connect(mongoAtlas, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=>{
+    console.log('masuk');
+  })
+  .catch(err=>{
+    console.log(err);
+  })
 
 
 app.get('/', (req, res, next)=>{
